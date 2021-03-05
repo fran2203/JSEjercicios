c98d22c4d651b9bdb804730f41866a8dae3d14d6
fran2203 <franciscoutrera@live.com>
2021-03-05 11:45:08 -0300

    Add ejercicio8/ejercio8.js

--- ejerciciosResueltos/ejercicio8/ejercicio8.js
+++ ejerciciosResueltos/ejercicio8/ejercicio8.js
@@ -1,10 +1,8 @@
 const deletePattern = (str, patt) => {
     if (!str && !patt) return console.warn('Debe llenar ambos campos');
 
-    let i = 0;
-    while (str.indexOf(patt, i) !== -1) {
-        if (str.indexOf(patt, i) !== -1) {
-            i = str.indexOf(patt, i) + 1;
-        }
-    }
+    const newStr = str.split(patt).join('');
+    return console.log(`Nueva frase: ${newStr}`);
 };
+
+deletePattern('xyz1, xyz2, xyz3, xyz4 y xyz5', 'xyz');
